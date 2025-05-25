9c3134fdd383d98bec00b3e8ab81c900ab91e07e
TashyaXD <anatasyadwirimarechiyono@gmail.com>
2025-05-25 23:30:32 +0700

    updateLearningPage

---
+++ src/scripts/pages/learning/LearningPage-Model.js
@@ -0,0 +1,28 @@
+export const articles = [
+  {
+    type: "article",
+    title: "Cara Memupuk Tanaman Secara Cepat dan Efektif",
+    videoUrl:
+      "https://r.search.yahoo.com/_ylt=AwrKB7pLMzNoRQIAUZzLQwx.;_ylu=Y29sbwNzZzMEcG9zAzIEdnRpZAMEc2VjA3Ny/RV=2/RE=1749395531/RO=10/RU=https%3a%2f%2fwww.nabilzaydan.com%2f2024%2f03%2fbudidaya-padi-organik-panduan-lengkap.html/RK=2/RS=SO79vbj1ngjrujkKh8nOq3kahM0-",
+  },
+  {
+    type: "video",
+    title: "Budidaya Tomat: Petunjuk, Persiapan, Pemupukan, Pemeliharaan",
+    videoUrl: "https://youtu.be/wiVJWdZANBs?si=jkZP1O4uFb7b4eyX",
+  },
+  {
+    type: "video",
+    title: "Mengenal 12 Jenis Tanaman Obat dan Manfaatnya",
+    videoUrl: "https://youtu.be/eXF-sRu4mYs?si=1VQR3j604cx0xN-h",
+  },
+  {
+    type: "video",
+    title: "Penyakit Tanaman: Pengertian, Gejala, Beserta Jenisnya",
+    videoUrl: "https://youtu.be/CVxlW51lZow?si=8VfzBShPaNmt8nZh",
+  },
+  {
+    type: "video",
+    title: "Cara Memupuk Tanaman Secara Cepat dan Efektif",
+    videoUrl: "https://youtu.be/111",
+  },
+];
